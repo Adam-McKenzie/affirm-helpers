d52a91fae5bc83da07e0ea77f348e0d3c7156a7c
Adam McKenzie <amckenzie@corp.ipipenet.com>
2016-09-13 10:21:36 -0400

    Updated TargetNodeReplacer | Added logging to output files

---
+++ CodeManagement/CMTestDriver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeManagement
+{
+    class CMTestDriver
+    {
+        const string replaceNodeInputPath = @"C:\TestFiles\CodeManagement\outputPath\fileNames.txt";
+        public void TestReplaceNodeBehavior(string inputPath)
+        {
+            TargetNodeReplacer replacer = new TargetNodeReplacer();
+            string fileName = null;
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(inputPath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    //get each file and replace target node if applicable
+                    fileName = reader.ReadLine();
+                    replacer.ReplaceTargetsWithImports(fileName);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            CMTestDriver driver = new CMTestDriver();
+            driver.TestReplaceNodeBehavior(replaceNodeInputPath);
+        }
+    }
+}
--- CodeManagement/TargetNodeReplacer.cs
+++ CodeManagement/TargetNodeReplacer.cs
@@ -10,8 +10,17 @@ namespace CodeManagement
     {
         const string importProjCommand = @"$(MSBuildProjectDirectory)\..\AdditionalFiles\AffirmBuild\AffirmBuild.Targets";
 
-        public void ReplaceTargetWithImport(string projectCommand, string filePath)
+        //TODO: Move this out as an input or member initialized by constructor
+        string changedFileOutputPath = @"C:\TestFiles\CodeManagement\outputPath\ReplaceNodeTestOutput.txt";
+        string errorFileOutputPath = @"C:\TestFiles\CodeManagement\outputPath\ReplaceNodeErrorTestOutput.txt";
+        string unchangedFileOutputPath = @"C:\TestFiles\CodeManagement\outputPath\ReplaceNodeUnchangedOutput.txt";
+
+
+        string savedFilePath = @"C:\Users\amckenzie\Documents\TestFiles\ADMServer2008\";
+
+        public void ReplaceTargetsWithImports(string fileName)
         {
+            string filePath = savedFilePath + fileName;
             //get file into an XDocument
             XDocument projectFile = null;
             using (System.IO.TextReader reader = new System.IO.StreamReader(filePath))
@@ -24,34 +33,49 @@ namespace CodeManagement
             //check for multiple target nodes TODO 
 
 
-            //TODO: should probably filter out files that do not meet this criteria 
+            //TODO: should probably filter out files that do not meet this criteria before we send to this method
             //if afterbuild predicate and where something references the nuget condition
             IEnumerable<XElement> possibleTargetNodes = projectFile.Root.Elements()
-                                                        .Where(x => x.Name.LocalName == "Target" && x.Attributes("Condition").Select(a => a.Value).FirstOrDefault() == @"$(Configuration)==NuGet");
+                                                        .Where(x => x.Name.LocalName == "Target" 
+                                                            && x.Attributes("Condition").Select(a => a.Value).FirstOrDefault() == @"$(Configuration)==NuGet"
+                                                            && x.Attributes("Name").Select(a => a.Value).FirstOrDefault() == "AfterBuild");
 
             //  <Target Name="AfterBuild" Condition="$(Configuration)==NuGet">
 
             //if count of node collection is zero, we don't need to bother
             if (possibleTargetNodes.Count() > 0)
             {
-                if (possibleTargetNodes.Count() > 1)
+                using (System.IO.TextWriter errorWriter = new System.IO.StreamWriter(errorFileOutputPath, true))
+                using (System.IO.TextWriter goodWriter = new System.IO.StreamWriter(changedFileOutputPath, true))
                 {
-                    
-                }
-                else
-                {
-                    //output to 
-                    XElement targetNode = null;
-                    //build import node
-                    XElement importNode = BuildImportNode(projectCommand);
-
-                    //replace target with import
-                    targetNode = importNode;
-                    //save the document to the same file path
-                    projectFile.Save(filePath);
+                    //output to a file and do nothing else
+                    if (possibleTargetNodes.Count() > 1)
+                    {
+                        //output to file and flag as a problem
+                        errorWriter.WriteLine(filePath);
+                    }
+                    else
+                    {
+                        //output to "replaced" file tracker
+                        goodWriter.WriteLine(filePath);
+                        XElement targetNode = possibleTargetNodes.FirstOrDefault();
+                        //build import node
+                        XElement importNode = BuildImportNode(importProjCommand);
+                        //replace target with import
+                        targetNode = importNode;
+                        //save the document to the same file path
+                        projectFile.Save(filePath);
+                    }
                 }
             }
-            
+            else
+            {
+                //for now we will track files that did not contain this node
+                using (System.IO.TextWriter unchangedWriter = new System.IO.StreamWriter(unchangedFileOutputPath,true))
+                {
+                    unchangedWriter.WriteLine(filePath);
+                }
+            }
         }
 
         private XElement BuildImportNode(string importProjCommand)
